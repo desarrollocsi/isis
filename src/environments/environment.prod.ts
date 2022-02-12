7da057742bb1438da6902f652812bb9ee281ff76
Saúl <saul.fujimoto@gmail.com>
2022-02-12 14:54:04 -0500

    enviroment produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://apps2.clinicasantaisabel.com',
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'http://apps2.clinicasantaisabel.com',
+  //apiUrl: 'http://127.0.0.1:8000',
 };
